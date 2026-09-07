496feafbe519ba5b73dd9e892891a96cd3b3e9c7
Krishnendu mandal <mandalkrishnendu1119@gmail.com>
2026-09-07 19:07:30 +0530

    Time: 20 ms (31.18%), Space: 48.2 MB (19.68%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int  minSpeed=1;
+        int maxSpeed =0;
+        for(int pile:piles){
+            maxSpeed=Math.max(pile,maxSpeed);
+        }
+
+        // binary search
+         while(minSpeed < maxSpeed){
+            int mid = minSpeed +(maxSpeed-minSpeed)/2;
+            if(canEat(piles,h,mid)){
+                maxSpeed=mid;
+            }else{
+                minSpeed = mid+1;
+            }
+         }
+         return minSpeed;
+    }
+
+    private boolean canEat(int[] piles, int h, int speed){
+        int hours=0;
+        for(int pile: piles){
+            hours+=(int)Math.ceil((double)pile/speed);
+
+            
+        }
+        return hours<=h;
+    }
+}
