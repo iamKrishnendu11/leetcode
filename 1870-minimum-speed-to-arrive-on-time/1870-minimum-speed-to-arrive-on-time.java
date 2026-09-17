f39ae7a305ca77b67c9a60dc9fb109d44611159a
Krishnendu mandal <mandalkrishnendu1119@gmail.com>
2026-09-17 23:14:25 +0530

    Time: 146 ms (9.57%), Space: 99.5 MB (41.96%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -3,17 +3,17 @@ class Solution {
         int low=1;
         int high = (int) 1e7;
 
-        int ans=-1;
-        while(low <= high){
-            int mid = low +(high-low)/2;
-            if(canReach(dist,hour,mid)){
-                ans = mid;
-                high = mid-1;
-            }else{
-                low = mid+1;
-            }
-        }
-        return ans;
+        while (low < high) {
+    int mid = low + (high - low) / 2;
+
+    if (canReach(dist, hour, mid)) {
+        high = mid;
+    } else {
+        low = mid + 1;
+    }
+}
+
+return canReach(dist, hour, low) ? low : -1;
     }
     private boolean canReach(int[] dist, double hour, int speed){
         double time=0.0;
